aff207f44c851923daf94c4f3d73373ee571f0f2
raibu <k561la3@meta.ua>
2020-05-08 20:56:14 +0300

    complete LAB6

--- lab6/src/lab.sv
+++ lab6/src/lab.sv
@@ -57,8 +57,51 @@ endmodule
 module microprog (
 	input clk,    // Clock
 	input rst_n,  // Asynchronous reset active low
-	
+	input [1:0] x,
+	output reg [3:0] y
 );
+
+	enum {Y0, Y1, Y2, Y3, Y4, Y5, Y6, Y7, Y8, Y9} state, next_state;
+
+	always_ff @(posedge clk or negedge rst_n) begin 
+		if(~rst_n) begin
+			state <= Y1;
+		end else begin
+			state <= next_state;
+		end
+	end
+
+	always_comb begin
+		case (state)
+			Y0: next_state = Y1;
+		 	Y1: next_state = Y2;
+		 	Y2: next_state = Y3;
+		 	Y3: next_state = x[0]?Y4:Y3;
+		 	Y4: next_state = Y5;
+		 	Y5: next_state = x[1]?Y8:Y6;
+		 	Y6: next_state = Y7;
+		 	Y7: next_state = Y9;
+		 	Y8: next_state = Y9;
+		 	Y9: next_state = Y0;
+			//implicit default x
+		endcase
+	end
+
+	always_comb begin
+		case (state)
+		 	Y0: y = 0;
+		 	Y1: y = 1;
+		 	Y2: y = 2;
+		 	Y3: y = 3;
+		 	Y4: y = 4;
+		 	Y5: y = 5;
+		 	Y6: y = 6;
+		 	Y7: y = 7;
+		 	Y8: y = 8;
+		 	Y9: y = 9;
+		 	default: y = 'bx;
+		endcase
+	end
 
 endmodule
 
--- lab6/src/test.sv
+++ lab6/src/test.sv
@@ -1,26 +1,40 @@
 `timescale 1ns/1ps
 
 module test;
-	reg clk, rst_n, w;
+	reg clk, frst_n, w;
 	wire out1;
 
-	automat fsm(clk, rst_n, w, out1);
+	automat fsm(clk, frst_n, w, out1);
 
 	reg[31:0] test_seq = 'b1010_1100_1110_0011_1100_0000_1111_1101;
 	//expected 			 ABFBFB FGBC FGHB CDFG HIBC DEEE FGHI IIBF
 	//out 				 000000 0000 0000 0000 0100 0111 0001 1100
+
+	reg mrst_n;
+	reg [1:0] x;
+	wire [3:0] y;
+	microprog mp(clk, mrst_n, x, y);
+
 	initial begin 
 		clk = '0;
 		forever #1 clk = !clk;
 	end
 
 	initial begin 
-		rst_n = 1;
+		mrst_n=0;
+		x=0;
+		frst_n = 1;
 		w=0;
-		#5 rst_n = 0;
-		#3 rst_n = 1;
+		#5 frst_n = 0;
+		#3 frst_n = 1;
 		repeat(32) #2 {w,test_seq} <= {test_seq, 1'b0};
-		#2 $finish;
+		frst_n=0;
+		#2 mrst_n = 1;
+		#10 x[0]=1;
+		#20 x[1]=1;
+		#20 x[0]=0;
+		#10 $finish;
+
 	end
 
 
