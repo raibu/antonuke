a59cdeb360609086646227a55ff55ca1a522f709
raibu <k561la3@meta.ua>
2020-05-08 15:11:55 +0300

    half LAB6

--- lab6/src/lab.sv
+++ lab6/src/lab.sv
@@ -2,29 +2,67 @@
 
 
 
-module lab2_1(in1, in2, in3, in4, out);
-input wire in1, in2, in3, in4;
-output wire out;	
+module automat (
+	input clk,    // Clock
+	input rst_n,  // Asynchronous reset active low
+	input w,
+	output logic out1
+);
 
-assign out =  ~( 							// nor3
-				(~( 						//nand2
-					(~(in1 & !in2)) & in3) 	//nand2
-				)|
-				(~(!in1 | in2 | !in3))| 	//nor3
-				(~(in1 | in4)) 				//nor2
-				);
+	enum {A, B, C, D, E, F, G, H, I} state, next_state;
+
+	always_ff @(posedge clk or negedge rst_n) begin 
+		if(~rst_n) begin
+			state <= A;
+		end else begin
+			state <= next_state;
+		end
+	end
+
+	always_comb begin
+		case (state)
+		 	A: next_state = w?F:B;
+		 	B: next_state = w?F:C;
+		 	C: next_state = w?F:D;
+		 	D: next_state = w?F:E;
+		 	E: next_state = w?F:E;
+		 	F: next_state = w?G:B;
+		 	G: next_state = w?H:B;
+		 	H: next_state = w?I:B;
+		 	I: next_state = w?I:B;
+			//implicit default x
+		endcase
+	end
+
+	always_comb begin
+		case (state)
+		 	A: out1 = 1'b0;
+		 	B: out1 = 1'b0;
+		 	C: out1 = 1'b0;
+		 	D: out1 = 1'b0;
+		 	E: out1 = 1'b1;
+		 	F: out1 = 1'b0;
+		 	G: out1 = 1'b0;
+		 	H: out1 = 1'b0;
+		 	I: out1 = 1'b1;
+		 	default: out1 = 1'bx;
+		endcase
+	end
 
 endmodule
 
 
 
-module lab2_2(x, y, z, out);
-input wire x, y, z;
-output wire out;	
 
-assign out =  (x & !y) | (y & z) | (x & !z);
+module microprog (
+	input clk,    // Clock
+	input rst_n,  // Asynchronous reset active low
+	
+);
 
 endmodule
+
+
 
 
 
--- lab6/src/test.sv
+++ lab6/src/test.sv
@@ -1,17 +1,28 @@
 `timescale 1ns/1ps
 
 module test;
-	reg [3:0] in;
-	wire out;
-	lab2_2 dut(in[0], in[1], in[2], out);
-	
-	integer i;
+	reg clk, rst_n, w;
+	wire out1;
+
+	automat fsm(clk, rst_n, w, out1);
+
+	reg[31:0] test_seq = 'b1010_1100_1110_0011_1100_0000_1111_1101;
+	//expected 			 ABFBFB FGBC FGHB CDFG HIBC DEEE FGHI IIBF
+	//out 				 000000 0000 0000 0000 0100 0111 0001 1100
 	initial begin 
-		for (i = 0; i < 8; i=i+1) begin
-			in <= i;
-			#4;
-		end
-	$finish;
+		clk = '0;
+		forever #1 clk = !clk;
 	end
+
+	initial begin 
+		rst_n = 1;
+		w=0;
+		#5 rst_n = 0;
+		#3 rst_n = 1;
+		repeat(32) #2 {w,test_seq} <= {test_seq, 1'b0};
+		#2 $finish;
+	end
+
+
 endmodule
 
