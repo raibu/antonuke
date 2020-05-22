91895329626a19c256371b1f58dc04b4760913a4
raibu <k561la3@meta.ua>
2020-05-22 20:55:57 +0300

    fixed indentation

--- lab7/src/lab.sv
+++ lab7/src/lab.sv
@@ -2,21 +2,21 @@
 
 
 module mf_alu #(parameter WIDTH = 16) (
-	input clk,    // Clock
-	input rst_n,  // Asynchronous reset active low
-	input signed [WIDTH-1:0] a1, a2, b1, b2,
-	input op, is_complex, use_cop,
-	output logic signed [WIDTH-1:0] out1, out2
+    input clk,    // Clock
+    input rst_n,  // Asynchronous reset active low
+    input signed [WIDTH-1:0] a1, a2, b1, b2,
+    input op, is_complex, use_cop,
+    output logic signed [WIDTH-1:0] out1, out2
 );
 
-	wire signed [WIDTH-1:0] res1_add, res2_add;  
-	wire signed [WIDTH-1:0] res1_cop, res2_cop;  
+    wire signed [WIDTH-1:0] res1_add, res2_add;  
+    wire signed [WIDTH-1:0] res1_cop, res2_cop;  
 
-	arithm #(WIDTH) ar1(clk, rst_n, a1, a2, b1, b2, op, res1_add, res2_add);
-	coproc #(WIDTH) cop(clk, rst_n, a1, a2, b1, b2, op, is_complex, res1_cop, res2_cop);
+    arithm #(WIDTH) ar1(clk, rst_n, a1, a2, b1, b2, op, res1_add, res2_add);
+    coproc #(WIDTH) cop(clk, rst_n, a1, a2, b1, b2, op, is_complex, res1_cop, res2_cop);
 
-	assign out1 = use_cop ? res1_cop : res1_add;
-	assign out2 = use_cop ? res2_cop : res2_add;
+    assign out1 = use_cop ? res1_cop : res1_add;
+    assign out2 = use_cop ? res2_cop : res2_add;
 
 
 
@@ -25,73 +25,73 @@ endmodule
 
 
 module arithm #(parameter WIDTH = 16) (
-	input clk,    // Clock
-	input rst_n,  // Asynchronous reset active low
-	input signed [WIDTH-1:0] a1, a2, b1, b2,
-	input addsub,
-	output logic signed [WIDTH-1:0] out1, out2
+    input clk,    // Clock
+    input rst_n,  // Asynchronous reset active low
+    input signed [WIDTH-1:0] a1, a2, b1, b2,
+    input addsub,
+    output logic signed [WIDTH-1:0] out1, out2
 );
-	reg signed [WIDTH-1:0] res1, res2; 
+    reg signed [WIDTH-1:0] res1, res2; 
 
-	assign res1 = addsub ? (a1-a2) : (a1+a2);
-	assign res2 = addsub ? (b1-b2) : (b1+b2);
-	
-	
-	always_ff @(posedge clk or negedge rst_n) begin
-		if(~rst_n) begin
-			{out2, out1} <= '0;
-		end else begin
-			out1 <= res1;
-			out2 <= res2;
-		end
-	end
+    assign res1 = addsub ? (a1-a2) : (a1+a2);
+    assign res2 = addsub ? (b1-b2) : (b1+b2);
+    
+    
+    always_ff @(posedge clk or negedge rst_n) begin
+        if(~rst_n) begin
+            {out2, out1} <= '0;
+        end else begin
+            out1 <= res1;
+            out2 <= res2;
+        end
+    end
 
 endmodule
 
 module coproc #(parameter WIDTH = 16) (
-	input clk,    // Clock
-	input rst_n,  // Asynchronous reset active low
-	input signed [WIDTH-1:0] a1, a2, b1, b2,
-	input muldiv, is_complex,
-	output logic signed [WIDTH-1:0] out1, out2
+    input clk,    // Clock
+    input rst_n,  // Asynchronous reset active low
+    input signed [WIDTH-1:0] a1, a2, b1, b2,
+    input muldiv, is_complex,
+    output logic signed [WIDTH-1:0] out1, out2
 );
-	reg signed [WIDTH-1:0] res1, res2; 
+    reg signed [WIDTH-1:0] res1, res2; 
 
-	always_comb begin 
-		case (is_complex)
-			0: 	case (muldiv)
-					0: 	begin 	
-							res1 = a1*a2;
-							res2 = b1*b2;
-						end
-					1:	begin
-							res1 = a1/a2;
-							res2 = b1/b2;
-						end
-				endcase
+    always_comb begin 
+        case (is_complex)
+            0:  case (muldiv)
+                    0:  begin   
+                            res1 = a1*a2;
+                            res2 = b1*b2;
+                        end
+                    1:  begin
+                            res1 = a1/a2;
+                            res2 = b1/b2;
+                        end
+                endcase
 
-			1:	case (muldiv)
-					0:  begin
-							res1 = (a1*a2) - (b1*b2); //re
-							res2 = (a1*b2) + (b1*a2);
-						end
-					1:	begin
-							res1 = ((a1*a2) + (b1*b2))/((a2*a2)+(b2*b2));
-							res2 = ((a2*b1) - (b2*a1))/((a2*a2)+(b2*b2));
-						end
-				endcase
-		endcase
-	end
+            1:  case (muldiv)
+                    0:  begin
+                            res1 = (a1*a2) - (b1*b2); //re
+                            res2 = (a1*b2) + (b1*a2);
+                        end
+                    1:  begin
+                            res1 = ((a1*a2) + (b1*b2))/((a2*a2)+(b2*b2));
+                            res2 = ((a2*b1) - (b2*a1))/((a2*a2)+(b2*b2));
+                        end
+                endcase
+        endcase
+    end
 
 
-	always_ff @(posedge clk or negedge rst_n) begin
-		if(~rst_n) begin
-			{out2, out1} <= '0;
-		end else begin
-			out1 <= res1;
-			out2 <= res2;
-		end
-	end
+    always_ff @(posedge clk or negedge rst_n) begin
+        if(~rst_n) begin
+            {out2, out1} <= '0;
+        end else begin
+            out1 <= res1;
+            out2 <= res2;
+        end
+    end
 
 
 endmodule
--- lab7/src/test.sv
+++ lab7/src/test.sv
@@ -8,143 +8,143 @@
 
 module test;
 
-	localparam WIDTH = 16;
-		
-	typedef struct packed{
-		bit signed [WIDTH-1:0] re, im;
-	}comp;
+    localparam WIDTH = 16;
+        
+    typedef struct packed{
+        bit signed [WIDTH-1:0] re, im;
+    }comp;
 
-	reg clk;   // Clock
-	reg rst_n; // Asynchronous reset active low
-	reg signed [WIDTH-1:0] a1, a2, b1, b2;
-	reg op, is_complex, use_cop;
-	wire signed [WIDTH-1:0] out1, out2;
-
-
-	covergroup cg();
-		option.per_instance = 1;
-		option.at_least = 3;
-	   	coverpoint op{
-	   		bins addmul = {0};
-	   		bins subdiv = {1};
-	   	}
-	   	coverpoint is_complex{
-	   		bins complex = {1};
-	   		bins coupled = {0};
-	   	}
-	   	coverpoint use_cop{
-	   		bins addsub = {0};
-	   		bins muldiv = {1};
-	   	}
-	   	coverpoint a1[WIDTH-1]{ bins plus = {0}; bins minus = {1}; }
-	   	coverpoint b1[WIDTH-1]{ bins plus = {0}; bins minus = {1}; }
-	   	coverpoint a2[WIDTH-1]{ bins plus = {0}; bins minus = {1}; }
-	   	coverpoint b2[WIDTH-1]{ bins plus = {0}; bins minus = {1}; }
-	endgroup : cg
-	cg cov;
-
-	mf_alu #(16) alu(clk, rst_n, a1, a2, b1, b2, op, is_complex, use_cop, out1, out2);
-	
-
-	initial begin 
-		clk = 0;
-		rst_n = 0;
-		forever #1 clk = !clk;
-	end
-
-	initial begin 
-		cov = new;
-		a1 = 0;
-		a2 = 0;
-		b1 = 0;
-		b2 = 0;
-		op = 0;
-		is_complex = 0;
-		use_cop = 0;
-		
-		repeat(2) @(posedge clk);
-		rst_n = 1;
+    reg clk;   // Clock
+    reg rst_n; // Asynchronous reset active low
+    reg signed [WIDTH-1:0] a1, a2, b1, b2;
+    reg op, is_complex, use_cop;
+    wire signed [WIDTH-1:0] out1, out2;
 
 
-		@(posedge clk);
-		repeat(100) begin 
-			a1 = $random(); 
-			b1 = $random(); 
-			a2 = $random(); 
-			b2 = $random(); 
-			op = $random();
-			is_complex = $random();
-			use_cop = $random();
+    covergroup cg();
+        option.per_instance = 1;
+        option.at_least = 3;
+        coverpoint op{
+            bins addmul = {0};
+            bins subdiv = {1};
+        }
+        coverpoint is_complex{
+            bins complex = {1};
+            bins coupled = {0};
+        }
+        coverpoint use_cop{
+            bins addsub = {0};
+            bins muldiv = {1};
+        }
+        coverpoint a1[WIDTH-1]{ bins plus = {0}; bins minus = {1}; }
+        coverpoint b1[WIDTH-1]{ bins plus = {0}; bins minus = {1}; }
+        coverpoint a2[WIDTH-1]{ bins plus = {0}; bins minus = {1}; }
+        coverpoint b2[WIDTH-1]{ bins plus = {0}; bins minus = {1}; }
+    endgroup : cg
+    cg cov;
+
+    mf_alu #(16) alu(clk, rst_n, a1, a2, b1, b2, op, is_complex, use_cop, out1, out2);
+    
+
+    initial begin 
+        clk = 0;
+        rst_n = 0;
+        forever #1 clk = !clk;
+    end
+
+    initial begin 
+        cov = new;
+        a1 = 0;
+        a2 = 0;
+        b1 = 0;
+        b2 = 0;
+        op = 0;
+        is_complex = 0;
+        use_cop = 0;
+        
+        repeat(2) @(posedge clk);
+        rst_n = 1;
 
 
-			@(posedge clk);
-			cov.sample();
-			$display("a1 %d, b1 %d, a2 %d, b2 %d, complex %b, cop %b, op %b",a1, b1, a2, b2, is_complex, use_cop, op);
+        @(posedge clk);
+        repeat(100) begin 
+            a1 = $random(); 
+            b1 = $random(); 
+            a2 = $random(); 
+            b2 = $random(); 
+            op = $random();
+            is_complex = $random();
+            use_cop = $random();
 
 
-			//assertions for numbers
-			if ({is_complex, use_cop, op} == 3'b000) assert ((out1 == a1+a2) && (out2 == b1+b2)) 
-				else $display("+ failed, expected %0x, got %0x", {a1+a2, b1+b2}, {out1, out2});
-
-			if ({is_complex, use_cop, op} == 3'b001) assert ((out1 == a1-a2) && (out2 == b1-b2)) 
-				else $display("- failed, expected %0x, got %0x", {a1-a2, b1-b2}, {out1, out2});
-
-			if ({is_complex, use_cop, op} == 3'b010) assert ((out1 == a1*a2) && (out2 == b1*b2)) 
-				else $display("* failed, expected %0x, got %0x", {a1*a2, b1*b2}, {out1, out2});
-
-			if ({is_complex, use_cop, op} == 3'b011) assert ((out1 == a1/a2) && (out2 == b1/b2)) 
-				else $display("/ failed, expected %0x, got %0x", {a1/a2, b1/b2}, {out1, out2});
+            @(posedge clk);
+            cov.sample();
+            $display("a1 %d, b1 %d, a2 %d, b2 %d, complex %b, cop %b, op %b",a1, b1, a2, b2, is_complex, use_cop, op);
 
 
-			//assertions for complex
-			if ({is_complex, use_cop, op} == 3'b100) assert ({out1,out2} == cadd(a1, a2, b1, b2)) 
-				else $display("c+ failed, expected %0x, got %0x", cadd(a1, a2, b1, b2), {out1, out2});
+            //assertions for numbers
+            if ({is_complex, use_cop, op} == 3'b000) assert ((out1 == a1+a2) && (out2 == b1+b2)) 
+                else $display("+ failed, expected %0x, got %0x", {a1+a2, b1+b2}, {out1, out2});
 
-			if ({is_complex, use_cop, op} == 3'b101) assert ({out1,out2} == csub(a1, a2, b1, b2)) 
-				else $display("c- failed, expected %0x, got %0x", csub(a1, a2, b1, b2), {out1, out2});
+            if ({is_complex, use_cop, op} == 3'b001) assert ((out1 == a1-a2) && (out2 == b1-b2)) 
+                else $display("- failed, expected %0x, got %0x", {a1-a2, b1-b2}, {out1, out2});
 
-			if ({is_complex, use_cop, op} == 3'b110) assert ({out1,out2} == cmul(a1, a2, b1, b2)) 
-				else $display("c* failed, expected %0x, got %0x", cmul(a1, a2, b1, b2), {out1, out2});
+            if ({is_complex, use_cop, op} == 3'b010) assert ((out1 == a1*a2) && (out2 == b1*b2)) 
+                else $display("* failed, expected %0x, got %0x", {a1*a2, b1*b2}, {out1, out2});
 
-			if ({is_complex, use_cop, op} == 3'b111) assert ({out1,out2} == cdiv(a1, a2, b1, b2)) 
-				else $display("c/ failed, expected %0x, got %0x", cdiv(a1, a2, b1, b2), {out1, out2});
-
-		end
-
-		$display("\033[31;1;4mCOVERAGE %f\033[0m",cov.get_coverage());
-		$finish;
-	end
+            if ({is_complex, use_cop, op} == 3'b011) assert ((out1 == a1/a2) && (out2 == b1/b2)) 
+                else $display("/ failed, expected %0x, got %0x", {a1/a2, b1/b2}, {out1, out2});
 
 
+            //assertions for complex
+            if ({is_complex, use_cop, op} == 3'b100) assert ({out1,out2} == cadd(a1, a2, b1, b2)) 
+                else $display("c+ failed, expected %0x, got %0x", cadd(a1, a2, b1, b2), {out1, out2});
+
+            if ({is_complex, use_cop, op} == 3'b101) assert ({out1,out2} == csub(a1, a2, b1, b2)) 
+                else $display("c- failed, expected %0x, got %0x", csub(a1, a2, b1, b2), {out1, out2});
+
+            if ({is_complex, use_cop, op} == 3'b110) assert ({out1,out2} == cmul(a1, a2, b1, b2)) 
+                else $display("c* failed, expected %0x, got %0x", cmul(a1, a2, b1, b2), {out1, out2});
+
+            if ({is_complex, use_cop, op} == 3'b111) assert ({out1,out2} == cdiv(a1, a2, b1, b2)) 
+                else $display("c/ failed, expected %0x, got %0x", cdiv(a1, a2, b1, b2), {out1, out2});
+
+        end
+
+        $display("\033[31;1;4mCOVERAGE %f\033[0m",cov.get_coverage());
+        $finish;
+    end
 
 
-	function comp cadd(input signed [WIDTH-1:0] re1, re2, im1, im2);
-		comp c;
-		c.re = re1+re2;
-		c.im = im1+im2;
-		return c;
-	endfunction : cadd
 
-	function comp csub(input signed [WIDTH-1:0] re1, re2, im1, im2);
-		comp c;
-		c.re = re1-re2;
-		c.im = im1-im2;
-		return c;
-	endfunction : csub
 
-	function comp cmul(input signed [WIDTH-1:0] re1, re2, im1, im2);
-		comp c;
-		c.re = re1*re2 - im1*im2;
-		c.im = re1*im2 + im1*re2;
-		return c;
-	endfunction : cmul
+    function comp cadd(input signed [WIDTH-1:0] re1, re2, im1, im2);
+        comp c;
+        c.re = re1+re2;
+        c.im = im1+im2;
+        return c;
+    endfunction : cadd
 
-	function comp cdiv(input signed [WIDTH-1:0] re1, re2, im1, im2);
-		comp c;
-		c.re = (re1*re2+im1*im2)/(re2*re2+im2*im2);
-		c.im = (im1*re2-re1*im2)/(re2*re2+im2*im2);
-		return c;
-	endfunction : cdiv
+    function comp csub(input signed [WIDTH-1:0] re1, re2, im1, im2);
+        comp c;
+        c.re = re1-re2;
+        c.im = im1-im2;
+        return c;
+    endfunction : csub
+
+    function comp cmul(input signed [WIDTH-1:0] re1, re2, im1, im2);
+        comp c;
+        c.re = re1*re2 - im1*im2;
+        c.im = re1*im2 + im1*re2;
+        return c;
+    endfunction : cmul
+
+    function comp cdiv(input signed [WIDTH-1:0] re1, re2, im1, im2);
+        comp c;
+        c.re = (re1*re2+im1*im2)/(re2*re2+im2*im2);
+        c.im = (im1*re2-re1*im2)/(re2*re2+im2*im2);
+        return c;
+    endfunction : cdiv
 
 
 
